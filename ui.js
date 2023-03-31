5b9dafef3d11d8128c77b0748a320e8d87d5b86c
NaimeDmts <naime.dmts@gmail.com>
2023-04-01 01:04:59 +0300

    4- Show Todo Items

--- ui.js
+++ ui.js
@@ -38,6 +38,10 @@ class UI{
                                 company : ${profile.company.name}
                             </li>
                         </ul>
+                        <h4 class="mt-4"> Todo List </h4>
+                        <ul id="todo" class="list-group">
+
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -45,6 +49,28 @@ class UI{
     }
     showAlert(text){
         this.alert.innerHTML = `${text} is not found.`;
+    }
+    showTodo(todo){
+        let html = "";
+
+        todo.forEach(item => {
+            if(item.completed){
+                html += `
+                    <li class="list-group-item bg-success">
+                        ${item.title}
+                    </li>
+                `
+            }
+            else{
+                html += `
+                    <li class="list-group-item bg-secondary">
+                        ${item.title}
+                    </li>
+                `
+            }
+        });
+
+        this.profileContainer.querySelector('#todo').innerHTML = html;
     }
 
     clear(){
